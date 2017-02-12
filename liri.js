4816805c3f06b2e006286888753040283f3ba1b7
hfung8 <harrison.hok.fung@gmail.com>
2017-02-12 16:06:09 -0500

    made changes to the twitter

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 
+var tweets = process.argv[2];
+
 process.env.TWITTER_CONSUMER_KEY = keys.twitterKeys.consumer_key;
 process.env.TWITTER_CONSUMER_SECRET = keys.twitterKeys.consumer_secret;
 process.env.TWITTER_ACCESS_TOKEN_KEY = keys.twitterKeys.access_token_key;
@@ -13,11 +15,21 @@ var client = new Twitter({
 	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+if (tweets === 'my-tweets'){
 
-var params = {screen_name: 'nodejs'};
-client.get('search/tweets', params, function(error, tweets, response) {
+function getTweets(){	
+var params = {screen_name: 'hfunger8'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-  	console.log("test");
-    console.log(tweets);
-  }
-});
+    console.log("test");
+    for (var i = 0; i < tweets.length; i++){
+    console.log(tweets[i].text);
+				}
+			}	
+		})
+	}
+	getTweets();
+};
+
+
+
